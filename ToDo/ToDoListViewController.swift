82a026bf1fdac90cc267bf7cab20654118f8a1f1
Cem Safa <cemsafa@gmail.com>
2020-07-18 09:42:00 +0300

    Implement Table View with dummy data

---
+++ ToDo/ToDoListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ToDoListViewController.swift
+//  ToDo
+//
+//  Created by Cem Safa on 18.07.2020.
+//  Copyright © 2020 Cem Safa. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos for Eleven", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+
+}
